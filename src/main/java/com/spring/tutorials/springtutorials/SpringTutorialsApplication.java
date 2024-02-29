c701589eaf84d199e4283bd348d68cc19ead6a5e
paul.rus <paul.rus@globant.com>
2024-02-29 12:11:35 +0200

    add test endpoint

---
+++ src/main/java/com/spring/tutorials/springtutorials/SpringTutorialsApplication.java
@@ -0,0 +1,14 @@
+package com.spring.tutorials.springtutorials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class SpringTutorialsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTutorialsApplication.class, args);
+	}
+
+}
